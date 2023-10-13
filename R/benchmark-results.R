324ffd47504503e6f3d4f6c3f18f70d1da3e726c
Austin Dickey <austin3spammy@gmail.com>
2023-10-13 10:38:34 -0500

    change benchmark_results to use pagination

--- R/benchmark-results.R
+++ R/benchmark-results.R
@@ -1,10 +1,47 @@
+.construct_request <- function(
+  run_id, batch_id, run_reason, name, earliest_timestamp, latest_timestamp, cursor
+) {
+  req <- req_url_path_append(conbench_request(), "benchmark-results")
+
+  if (!is.null(run_reason)) {
+    req <- req_url_query(req, run_reason = run_reason)
+  }
+
+  if (!is.null(batch_id)) {
+    req <- req_url_query(req, batch_id = batch_id)
+  }
+
+  if (!is.null(run_id)) {
+    req <- req_url_query(req, run_id = run_id)
+  }
+
+  if (!is.null(name)) {
+    req <- req_url_query(req, name = name)
+  }
+
+  if (!is.null(earliest_timestamp)) {
+    req <- req_url_query(req, earliest_timestamp = earliest_timestamp)
+  }
+
+  if (!is.null(latest_timestamp)) {
+    req <- req_url_query(req, latest_timestamp = latest_timestamp)
+  }
+
+  if (!is.null(cursor)) {
+    req <- req_url_query(req, cursor = cursor)
+  }
+
+  req
+}
+
 #' Get a list of benchmark results
 #'
 #' @param run_id the run_id to get benchmarks for (default: `NULL`, list all)
 #' @param batch_id the batch_id to get benchmarks for (default: `NULL`, list all)
 #' @param run_reason a string to specify the run reason (default: `NULL`, list all)
-#' @param limit the number of results to return for a specific `run_reason` is also specified (default: `NULL`, list all)
-#' @param days the number of days to look back from today for benchmark results. (default: `NULL`, list all)
+#' @param name the conceptual benchmark name (default: `NULL`, list all)
+#' @param earliest_timestamp the earliest benchmark result timestamp (default: `NULL`, go back as far as possible)
+#' @param latest_timestamp the latest benchmark result timestamp (default: `NULL`, go up to the current time)
 #' @inheritParams runs
 #'
 #' @return a data.frame of benchmark results
@@ -13,60 +50,52 @@ benchmark_results <- function(
     run_id = NULL,
     batch_id = NULL,
     run_reason = NULL,
-    limit = NULL,
-    days = NULL,
+    name = NULL,
+    earliest_timestamp = NULL,
+    latest_timestamp = NULL,
     simplifyVector = TRUE,
     flatten = TRUE,
     ...) {
-  ## Assert that run_id is a vector of length 5 or less
-  if (length(unique(run_id)) > 5) {
-    stop("Too many run_ids, please limit to 5 or less or consider making separate requests.", call. = FALSE)
+  ## Assert that run_id is a string
+  if (length(run_id) > 1) {
+    stop("Too many run_ids, please limit to 1.", call. = FALSE)
   }
 
-  ## Assert that only one of run_id, batch_id or run_reason is non-NULL
-  non_null_count <- Filter(Negate(is.null), list(run_id, batch_id, run_reason))
-
-  if (length(non_null_count) >= 2) {
-    stop("Only one of run_id, batch_id or run_reason can be non-NULL", call. = FALSE)
-  }
-
-  req <- req_url_path_append(conbench_request(), "benchmark-results")
-
-  if (!is.null(run_reason)) {
-    req <- req_url_query(req, run_reason = run_reason)
-  }
-
-  if (!is.null(days)) {
-    days <- integer_messager(days, label = "days")
-    req <- req_url_query(req, days = days)
-  }
-
-  if (!is.null(limit) && !is.null(run_reason)) {
-    limit <- integer_messager(limit, label = "limit")
-    req <- req_url_query(req, limit = limit)
-  }
-  
-  if (!is.null(limit) && is.null(run_reason)) {
-    stop("you are setting a limit without setting run_reason. please set run_reason and try again", call. = TRUE)
-  }
-
-  if (!is.null(batch_id)) {
-    req <- req_url_query(req, batch_id = paste0(batch_id, collapse = ","))
-  }
-
-  if (!is.null(run_id)) {
-    req <- req_url_query(req, run_id = paste0(run_id, collapse = ","))
-  }
-
+  req <- .construct_request(
+    run_id = run_id,
+    batch_id = batch_id,
+    run_reason = run_reason,
+    name = name,
+    earliest_timestamp = earliest_timestamp,
+    latest_timestamp = latest_timestamp,
+    cursor = NULL
+  )
   resp <- conbench_perform(req)
+  json <- resp_body_json(resp, simplifyVector = simplifyVector, flatten = flatten, ...)
+  data <- json[["data"]]
 
-  resp_body_json(resp, simplifyVector = simplifyVector, flatten = flatten, ...)
+  while (!is.null(json[["metadata"]][["next_page_cursor"]])) {
+    req <- .construct_request(
+      run_id = run_id,
+      batch_id = batch_id,
+      run_reason = run_reason,
+      name = name,
+      earliest_timestamp = earliest_timestamp,
+      latest_timestamp = latest_timestamp,
+      cursor = json[["metadata"]][["next_page_cursor"]]
+    )
+    resp <- conbench_perform(req)
+    json <- resp_body_json(resp, simplifyVector = simplifyVector, flatten = flatten, ...)
+    data <- rbind(data, json[["data"]])
+  }
+
+  data
 }
 
-#' `benchmarks` is deprecated method of getting benchmark results and will be 
+#' `benchmarks` is deprecated method of getting benchmark results and will be
 #' retired in a future release. Please use `benchmark_results` instead.
 #' @rdname benchmark_results
-#' @export 
+#' @export
 benchmarks <- function(run_id = NULL, batch_id = NULL, run_reason = NULL, simplifyVector = TRUE, flatten = TRUE, ...) {
   .Deprecated("benchmark_results")
 
