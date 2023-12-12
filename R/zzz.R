4dcfaac6d97d15e95298ac21052acf86589db98d
Sam Albers <sam.albers@gmail.com>
2023-12-12 11:03:00 -0800

    fix msg

--- R/zzz.R
+++ R/zzz.R
@@ -1,16 +1,19 @@
 .onAttach <- function(libname, pkgname) {
   if (interactive()) {
-    load_msg <- tryCatch({
-      msg <- conbench_info()
-      cat(
-        paste0(
-            "Conbench server information\n", 
-            paste0(" - ", names(msg),": ", msg, collapse = "\n")
-            ), 
-            "\n")
-    }, error = function(e) {
-      "No available conbench info"
-    })
-    return(packageStartupMessage(load_msg))
+    load_msg <- tryCatch(
+      {
+        msg <- conbench_info()
+        packageStartupMessage(
+            paste0(
+              "Conbench server information\n",
+              paste0(" - ", names(msg), ": ", msg, collapse = "\n")
+            ),
+            "\n"
+          )
+      },
+      error = function(e) {
+        packageStartupMessage("No available conbench info")
+      }
+    )
   }
 }
