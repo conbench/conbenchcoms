73564326e173801dc6151cf57aebc993ec9deb95
Sam Albers <sam.albers@gmail.com>
2023-12-12 11:18:04 -0800

    try different endpoint

--- tests/testthat/test-session.R
+++ tests/testthat/test-session.R
@@ -35,10 +35,10 @@ with_mock_dir(test_path("not-logged-in"), {
   test_that("Can log in", {
     expect_null(.conbench_session$cookie)
     resp <- request("http://localhost/api") |>
-      req_url_path_append("users") |>
+      req_url_path_append("ping") |>
       conbench_perform()
     # This is still a 401 becuase httptest2 only has one possible response for users
-    # expect_identical(resp_status(resp), 401L)
+    expect_identical(resp_status(resp), 401L)
     expect_identical(.conbench_session$cookie, "REDACTED")
   })
 })
